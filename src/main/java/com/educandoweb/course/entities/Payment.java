b58b9a37554c7d101d051581664788ee6ca56fd2
Leonardo Moreira <lemoreir@gmail.com>
2026-01-14 18:45:09 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,7 @@ public class Payment implements Serializable {
 	//Entre payment e order, payment é a classe dependente, então vai a notation @MapsId
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {
