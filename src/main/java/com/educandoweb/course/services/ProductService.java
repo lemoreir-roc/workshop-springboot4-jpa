435ec37a47d927310281cee0f52b89346927a733
Leonardo Moreira <lemoreir@gmail.com>
2026-01-26 01:21:50 -0300

    Product update

--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -2,15 +2,20 @@ package com.educandoweb.course.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.ProductRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 //2 operações - buscar todos os usuarios e buscar usuario por Id
 @Service
@@ -18,6 +23,11 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	 @Autowired
+	private CategoryRepository categoryRepository;
+	 
+	 
 	
 	public List<Product> findAll() {
 		return repository.findAll();
@@ -40,5 +50,38 @@ public class ProductService {
             ));
         
         repository.delete(product);
+    }
+	
+	public Product update(Long id, Product obj) {
+		try {
+		Product entity = repository.getReferenceById(id);
+		updateData(entity, obj); //Metodo criado abaixo
+		updateCategories(entity, obj.getCategories());
+		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product obj) {
+		//Só iremos atualizar o nome, email e telefone
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+	}
+	
+	private void updateCategories(Product entity, Set<Category> newCategories) {
+        // Limpa as categorias existentes
+        entity.getCategories().clear();
+        
+        // Para cada categoria recebida no JSON
+        for (Category cat : newCategories) {
+            // Busca a categoria no banco para garantir que existe
+            Category category = categoryRepository.findById(cat.getId())
+                .orElseThrow(() -> new ResourceNotFoundException(cat.getId()));
+            
+            // Adiciona a categoria ao produto
+            entity.getCategories().add(category);
+        }
     }
 }
